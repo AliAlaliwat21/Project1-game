639acc0182e945aaf91e9f59977980dfa4dada75
Ali <Alialaliwat21@gmail.com>
2026-06-29 09:28:50 +0300

    connected html elements to JS

---
+++ app.js
@@ -0,0 +1,13 @@
+/*-------------------------------- Constants --------------------------------*/
+const elstartbutton = document.querySelector('#start-btn')
+const elhitbutton = document.querySelector('#hit-btn')
+const elstandbutton = document.querySelector('#stand-btn')
+const elplayagainbutton = document.querySelector('#play-again-btn')
+const elresetbutton = document.querySelector('#reset-btn')
+/*-------------------------------- Variables --------------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
