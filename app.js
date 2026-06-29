53fbc93c5c336f660abf91ecb13fd5c5d294f28f
Ali <Alialaliwat21@gmail.com>
2026-06-29 10:31:50 +0300

    connected all variables from html to js fully and fixed HTML variables

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 /*-------------------------------- Constants --------------------------------*/
-const elstartbutton = document.querySelector('#start-btn')
-const elhitbutton = document.querySelector('#hit-btn')
-const elstandbutton = document.querySelector('#stand-btn')
-const elplayagainbutton = document.querySelector('#play-again-btn')
-const elresetbutton = document.querySelector('#reset-btn')
+
 /*-------------------------------- Variables --------------------------------*/
 let playerHand = []
 let dealerhand = []
@@ -16,7 +12,21 @@ let dealerTotal = 0
 
 let RoundOver = false
 /*------------------------ Cached Element References ------------------------*/
+const elstartbutton = document.querySelector('#start-btn')
+const elhitbutton = document.querySelector('#hit-btn')
+const elstandbutton = document.querySelector('#stand-btn')
+const elplayagainbutton = document.querySelector('#play-again-btn')
+const elresetbutton = document.querySelector('#reset-btn')
 
+const messageEl = document.querySelector('#message')
+const playerWinsEl = document.querySelector('#player-wins')
+const dealerWinsEl = document.querySelector('#dealer-wins')
+
+const playerTotalEl = document.querySelector('#player-total')
+const dealerTotalEl = document.querySelector('#dealer-total')
+
+const playercardsEl = document.querySelector('#player-cards')
+const dealerCardsEl = document.querySelector('#dealer-cards')
 /*----------------------------- Event Listeners -----------------------------*/
 
 /*-------------------------------- Functions --------------------------------*/
