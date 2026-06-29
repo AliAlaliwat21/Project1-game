94163e0a099e271c34d079b5dac9517f7763c8ce
Ali <Alialaliwat21@gmail.com>
2026-06-29 09:55:24 +0300

    created blackjack JS variables

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const elstandbutton = document.querySelector('#stand-btn')
 const elplayagainbutton = document.querySelector('#play-again-btn')
 const elresetbutton = document.querySelector('#reset-btn')
 /*-------------------------------- Variables --------------------------------*/
+let playerHand = []
+let dealerhand = []
 
+let playerWins = 0
+let dealerWins = 0
+
+let playerTotal = 0
+let dealerTotal = 0
+
+let RoundOver = false
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
